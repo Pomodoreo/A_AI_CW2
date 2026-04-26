169481cad9135accc85c6146d4e0c7ed101c067f
Ethan Tangka <113841725+Pomodoroeo@users.noreply.github.com>
2026-04-26 13:44:43 +0100

    Initial api code

--- ticket.py
+++ ticket.py
@@ -1,8 +1,54 @@
 # This will be used for getting the ticket for the API
 
 from zeep import Client
+from zeep.transports import Transport
+from requests import Session
+from requests.auth import HTTPBasicAuth
+from datetime import datetime
 
-client = Client('jpservices.wsdl.xml')
+session = Session()
+session.auth = HTTPBasicAuth('etangka', '6Ci0#3')
+
+# session.headers.update({
+#     'SOAPAction': 'RealtimeJourneyPlan'
+# })
+
+
+client = Client(
+    'jpservices.wsdl.xml',
+    transport=Transport(session=session)
+)
+
+service = client.create_service(
+    '{http://ojp.nationalrail.co.uk}jpservicesBinding',
+    'https://ojp.nationalrail.co.uk/webservices/jpdlr'
+)
+
+
+response = client.service.RealtimeJourneyPlan(
+    origin={'stationCRS': 'NRW'},
+    destination={'stationCRS': 'LST'},
+    realtimeEnquiry='STANDARD',
+    outwardTime={'departBy': datetime(2026, 4, 27, 10, 30)},
+    directTrains=False
+)
+
+print(response)
+
+
+# print(client.wsdl.services)
+# print(client.service._operations)
+
+
+
+
+
+
+
+
+
+
+
 
 #result = client.service.SomeMethod()
-print(client.wsdl.dump())
+#print(client.wsdl.dump())
